e4b05981f16b66c146a83a0cb580f0f560bd5764
boreq <boreq@sourcedrops.com>
2014-05-25 19:00:44 +0200

    Added board statistics. Statistics can be accessed with a link placed in the footer in the board view. Statistics are automatically updated, data is retrieved by an asynchronous request.
    $ Fixed layout issue (add tag input).

--- archive_chan/urls.py
+++ archive_chan/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^board/(?P<name>[a-z]+)/$', views.BoardView.as_view(), name='board'),
     url(r'^board/(?P<name>[a-z]+)/thread/(?P<number>[0-9]+)/$', views.ThreadView.as_view(), name='thread'),
+    url(r'^board/(?P<name>[a-z]+)/stats/$', views.board_stats, name='board_stats'),
+
+    url(r'^ajax/board/(?P<name>[a-z]+)/stats/$', views.ajax_board_stats, name='ajax_board_stats'),
 
     url(r'^ajax/save/$', views.ajax_save_thread, name='ajax_save_thread'),
     url(r'^ajax/get_parent_thread/$', views.ajax_get_parent_thread, name='ajax_get_parent_thread'),
