a4d9a4023c78a5f3bf0a222cd7208eab931d5928
boreq <boreq@sourcedrops.com>
2014-05-27 17:32:47 +0200

    Cleaned up the repository, updated screenshots.
    + make.py is more versatile.
    + tools/ directory.
    * screens/ moved to docs/.
    * Static files are grouped into additional subfolders:
        src - source files which are minified/concatenated into files included in the templates
        lib - files which are a part of the external plugins or libraries

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Archive Chan
 Archive Chan is a 4chan archive implemented as a Django application.
 
-![Board view - catalog](screens/readme.png "Board view - catalog")
+![Board view - catalog](docs/screens/readme.png "Board view - catalog")
 
 ## Installation
 ### Required software versions
-Application was written with Python 3 and Django 1.6+ in mind. At this point in does not support Python 2.
+Application was written with Python 3 and Django 1.6+ in mind. At this point it does not support Python 2.
 
 ### Dependencies
-This application requires following packages:
+This application requires the following packages:
 
 * [requests](http://docs.python-requests.org/en/latest/)
 * [tendo](https://pypi.python.org/pypi/tendo/0.2.4)
@@ -24,11 +24,11 @@ Django commands `archive_chan_update` and `archive_chan_remove_old_threads` must
 WARNING: first `archive_chan_update` will take a lot of time to complete because it will have to scrap all threads in the specified boards. You might want to run it manually a couple of times in a row with `--progress` flag to see what is going on. After the command will finally take relatively short time to execute enable CRON and don't worry about it anymore.
 
 ## Usage
-Settings described here are added through the Django administration panel. You will see a lot of irrelevant tables there with debug mode enabled. You might want to disable it.
+Actions described here are executed through the Django administration panel. You will see a lot of irrelevant tables there with debug mode enabled so you might want to disable it.
 
-First you have to specify the boards which are supposed to be archived. Threads are updated in the specified intervals: script downloads a catalog for all specified boards, checks which threads have to be updated and adds new posts to the database. Old threads will be removed after the time specified in the board settings. Saved threads will be preserved and will not be deleted. You can also specify triggers which will automatically tag or save a thread if specified conditions arise. Logged in users might manually save threads and add or remove tags through the thread view.
+First you have to specify the boards which are supposed to be archived. Threads are updated in the specified intervals: script downloads a catalog for each of the specified boards, checks which threads have to be updated and adds new posts to the database. Old threads will be removed after the time specified in the board settings. Saved threads will be preserved and will not be deleted. You can also specify triggers which will automatically tag or save a thread if specified conditions arise. Logged in users might manually save threads and add or remove tags through the thread view.
 
 ## Changing the code
 All code improvements are greatly appreciated.  
 
-You have to run `make.py` after changing static JS or CSS files. Required files will be preprocessed, minified and concatenated. This script requires `yui-compressor`, `sass` and `cat`.
+You have to run `tools/make.py` after changing static JS or CSS files. Required files will be preprocessed, minified and concatenated. This script requires `yui-compressor`, `sass` and `cat`.
---
+++ tools/make.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+import os, argparse, json, sys
+
+# Parse the arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--makefile', help='Path to a makefile relative to the current working directory.', default='makefile.json')
+parser.add_argument('-d', '--dryrun', help='Don\'t execute any commands.', action='store_true')
+parser.add_argument('--verbose', help='Increase verbosity', action='store_true')
+args = parser.parse_args()
+
+# Establish the path to the makefile and its directory.
+makefile_path = os.path.join(os.getcwd(), args.makefile)
+makefile_dir = os.path.dirname(makefile_path)
+
+if args.verbose:
+    print(args)
+    print('Makefile path: %s' % makefile_path)
+    print('Makefile dir: %s' % makefile_dir)
+
+# Load json data.
+makefile=open(makefile_path)
+makefile_data = json.load(makefile)
+makefile.close()
+
+def replace_extension(file_path, current_ext, future_ext):
+    return file_path[:-len(current_ext)] + future_ext
+
+def get_full_path(file_path):
+    return os.path.abspath(os.path.join(makefile_dir, makefile_data['path'], file_path))
+
+def find_tool(file_path):
+    output_extension = None
+
+    for chain in makefile_data['chains']:
+        for chain_element in reversed(chain):
+            if file_path.endswith(chain_element['extension']):
+                return (chain_element['tool'], chain_element['extension'], output_extension)
+
+            output_extension = chain_element['extension']
+
+    return None
+
+def get_conversion_chain(file_path):
+    chain = []
+
+    while True:
+        tool, current_extension, output_extension = find_tool(file_path)
+
+        if tool is None:
+            break
+        else:
+            output_file_path = replace_extension(file_path, current_extension, output_extension)
+            chain.append((file_path, output_file_path, tool))
+            file_path = output_file_path
+
+    return chain
+
+def process_file(file_path):
+    print('\nPROCESS: %s' % file_path)
+
+    conversion_chain = get_conversion_chain(file_path)
+
+    for conversion in conversion_chain:
+        print('  CONVERT %s TO %s WITH %s' % conversion)
+        command = format(conversion[2] % (get_full_path(conversion[0]), get_full_path(conversion[1])))
+        if args.verbose:
+            print(command)
+        if not args.dryrun:
+            if os.system(command) != 0:
+                sys.exit(1)
+
+    if len(conversion_chain) > 0:
+        return conversion_chain[-1][1]
+    else:
+        return file_path
+
+def get_intermediate_files(file_path):
+    return [conversion[1] for conversion in get_conversion_chain(file_path)]
+
+def handle(task):
+    final_files = []
+
+    # Process files.
+    for file_path in task['input']:
+        final_files.append(process_file(file_path))
+    
+    final_files = [get_full_path(file_path) for file_path in final_files]
+
+    # Concat final files.
+    print('\nCONCAT\n')
+    command = format("cat %s > %s" % (" ".join(final_files), get_full_path(task['output'])))
+    if args.verbose:
+        print(command)
+    if not args.dryrun:
+        if os.system(command) != 0:
+            sys.exit(1)
+
+    # Remove intermediate files.
+    for file_path in task['input']:
+        for intermediate_file in get_intermediate_files(file_path):
+            print('REMOVE %s' % intermediate_file)
+            remove_path = get_full_path(intermediate_file)
+            if args.verbose:
+                print(remove_path)
+            if not args.dryrun:
+                try:
+                    os.remove(remove_path)
+                except:
+                    pass
+
+for task in makefile_data['tasks']:
+    handle(task)
