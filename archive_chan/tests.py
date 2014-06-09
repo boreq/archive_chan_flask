7d0ed29b71ec2c41c3491e7477b7062af2d256f1
boreq <boreq@sourcedrops.com>
2014-06-10 00:06:54 +0200

    Added modifiers tests.

--- archive_chan/tests.py
+++ archive_chan/tests.py
@@ -1,3 +1,81 @@
 from django.test import TestCase
 
-# Create your tests here.
+import archive_chan.lib.modifiers as modifiers
+
+class SimpleFilterTest(TestCase):
+    def setUp(self):
+        self.parameters = (
+            ('default', ('Default', None)),
+            ('option', ('Option', {'field': True})),
+        )
+
+    def test_none(self):
+        modifier = modifiers.SimpleFilter(
+            self.parameters,
+            None
+        )
+        self.assertEqual(modifier.get(), 'default')
+
+    def test_default(self):
+        modifier = modifiers.SimpleFilter(
+            self.parameters,
+            'default'
+        )
+        self.assertEqual(modifier.get(), 'default')
+
+    def test_option(self):
+        modifier = modifiers.SimpleFilter(
+            self.parameters,
+            'option'
+        )
+        self.assertEqual(modifier.get(), 'option')
+
+    def test_wrong(self):
+        modifier = modifiers.SimpleFilter(
+            self.parameters,
+            'wrong_value'
+        )
+        self.assertEqual(modifier.get(), 'default')
+
+
+class SimpleSortTest(TestCase):
+    def setUp(self):
+        self.parameters = (
+            ('default', ('Last reply', 'field', None)),
+            ('option', ('Creation date', 'other_field', {'annotate': 'field'})),
+        )
+
+    def test_none(self):
+        modifier = modifiers.SimpleSort(
+            self.parameters,
+            None
+        )
+        self.assertEqual(modifier.get(), ('default', True))
+
+    def test_default(self):
+        modifier = modifiers.SimpleSort(
+            self.parameters,
+            'default'
+        )
+        self.assertEqual(modifier.get(), ('default', False))
+
+    def test_option(self):
+        modifier = modifiers.SimpleSort(
+            self.parameters,
+            'option'
+        )
+        self.assertEqual(modifier.get(), ('option', False))
+
+    def test_option_reverse(self):
+        modifier = modifiers.SimpleSort(
+            self.parameters,
+            '-option'
+        )
+        self.assertEqual(modifier.get(), ('option', True))
+
+    def test_wrong(self):
+        modifier = modifiers.SimpleSort(
+            self.parameters,
+            'wrong_value'
+        )
+        self.assertEqual(modifier.get(), ('default', True))
