575e80a5aa1b0233b93a723a8bf25469ef687a84
boreq <boreq@sourcedrops.com>
2014-06-30 21:47:34 +0200

    Added unit tests: views and status api.

--- archive_chan/tests.py
+++ archive_chan/tests.py
@@ -1,6 +1,14 @@
+import datetime, json
+
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
+from django.utils.timezone import utc
 
 import archive_chan.lib.modifiers as modifiers
+import archive_chan.models as models
+
+now = datetime.datetime(2014, 4, 23, 15, 0, 0, 0, utc)
 
 class SimpleFilterTest(TestCase):
     def setUp(self):
@@ -79,3 +87,92 @@ class SimpleSortTest(TestCase):
             'wrong_value'
         )
         self.assertEqual(modifier.get(), ('default', True))
+
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.views = [
+            ('index', ()),
+            ('stats', ()),
+            ('gallery', ()),
+            ('search', ()),
+            ('status', ()),
+        ]
+
+        self.client = Client()
+
+    def test_views(self):
+        """Check if views return status code 200. AJAX/API views are not tested here."""
+        def test_list(view_list):
+            for view in view_list:
+                response = self.client.get(reverse('archive_chan:%s' % view[0], args=view[1]))
+                self.assertEqual(response.status_code, 200)
+
+        # Empty.
+        test_list(self.views)
+
+        board = models.Board.objects.create(name='a')
+
+        self.views.extend([
+            ('board', (board.name)),
+            ('board_stats', (board.name)),
+            ('board_gallery', (board.name)),
+            ('board_search', (board.name)),
+        ])
+
+        # Board only.
+        test_list(self.views)
+
+        thread = models.Thread.objects.create(board=board, number=1, first_reply=now, last_reply=now)
+
+        self.views.extend([
+            ('thread', (board.name, thread.number)),
+            ('thread_stats', (board.name, thread.number)),
+            ('thread_gallery', (board.name, thread.number)),
+            ('thread_search', (board.name, thread.number)),
+        ])
+
+        # Board and empty thread.
+        test_list(self.views)
+
+        post = models.Post.objects.create(thread=thread, number=1, time=now)
+
+        # Board and thread with one post.
+        test_list(self.views)
+
+
+class ApiTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.board_a = models.Board.objects.create(name='a')
+        self.board_b = models.Board.objects.create(name='b')
+
+    def get_api(self, url):
+        response = self.client.get(url)
+        response_data = json.loads(response.content.decode())
+        return (response, response_data)
+
+    def test_status(self):
+        # Check for empty db table.
+        response, response_data = self.get_api(reverse('archive_chan:api_status'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('last_updates' in response_data and len(response_data['last_updates']) == 0, True)
+        self.assertEqual('chart_data' in response_data and len(response_data['chart_data']['rows']) == 0, True)
+
+        # Check for one ongoing update.
+        update1 = models.Update.objects.create(board=self.board_a, start=now, end=now, status=models.Update.CURRENT, used_threads = 1)
+
+        response, response_data = self.get_api(reverse('archive_chan:api_status'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('last_updates' in response_data and len(response_data['last_updates']) == 1, True)
+        self.assertEqual('chart_data' in response_data and len(response_data['chart_data']['rows']) == 0, True)
+
+        # Check for multiple different updates.
+        update2 = models.Update.objects.create(board=self.board_a, start=now, end=now, status=models.Update.COMPLETED, used_threads = 1)
+        update3 = models.Update.objects.create(board=self.board_b, start=now, end=now, status=models.Update.FAILED, used_threads = 1)
+
+        response, response_data = self.get_api(reverse('archive_chan:api_status'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('last_updates' in response_data and len(response_data['last_updates']) == 2, True)
+        self.assertEqual('chart_data' in response_data and len(response_data['chart_data']['rows']) == 1, True)
