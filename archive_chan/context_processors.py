c493d1f32e067a1877e96bc110642227970824e8
boreq <boreq@sourcedrops.com>
2014-08-22 21:52:04 +0200

    Cleaned up context_processors.py.

--- archive_chan/context_processors.py
+++ archive_chan/context_processors.py
@@ -23,32 +23,28 @@ def now():
 
 @bl.app_context_processor
 def user():
-    """Injects the object used by flask-login extension to represent
-    the current user.
+    """Injects the object used by flask-login extension to represent the
+    current user.
     """
     return dict(user=current_user)
 
 
 @bl.app_context_processor
 def board_url_query():
-    """Injects method used to build the query part of the board url."""
+    """Injects the method used to build the query part of the board url."""
     def url_query(parameters, name=None, value=None):
         parameters = copy.copy(parameters)
         if name:
             parameters[name] = value
-
         query =  '?sort=%s&saved=%s&last_reply=%s&tagged=%s' % (
             parameters['sort_with_operator'],
             parameters['saved'],
             parameters['last_reply'],
             parameters['tagged'],
         )
-
         if parameters['tag'] is not None:
-            query += '&tag=%s' % (
-                urllib.parse.quote('+'.join(parameters['tag']))
-            )
-
+            tags = urllib.parse.quote('+'.join(parameters['tag']))
+            query += '&tag=%s' % tags
         return query
 
     return dict(board_url_query=url_query)
